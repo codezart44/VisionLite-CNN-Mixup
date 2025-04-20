4d5b9b385a6ac09be9f81c5f92a1a99b5c62593e
oskarew <oswa.public@gmail.com>
2025-04-20 20:01:15 +0545

    minor

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -2,3 +2,4 @@ from .baseline_cnn import BaselineCNN
 from .lenet5 import LeNet5
 from .resnet_mini import ResNetMini
 from .densenet_tiny import DenseNetTiny
+from .mobilenet_lite import MobileNetLite
