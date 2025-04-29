2de82f46e494e6d28ed3b47c1c520ac77cd5998c
oskarew <oswa.public@gmail.com>
2025-04-29 11:16:13 +0800

    early stopper moved to separate file for modularity

---
+++ src/utils/early_stopping.py
@@ -0,0 +1,21 @@
+
+class EarlyStopper:
+    """ Early Stopper """
+    def __init__(self, patience: int = 5):
+        self.patience = patience
+        self.best_loss = float('inf')
+        self.counter = 0
+
+    def check_stop(self, test_loss: float) -> bool:
+        """..."""
+        if test_loss < self.best_loss:
+            self.best_loss = test_loss
+            self.counter = 0
+        else:
+            self.counter += 1
+        return self.counter >= self.patience
+    
+    def reset_stopper(self) -> None:
+        """..."""
+        self.best_loss = float('-inf')
+        self.counter = 0
