aaeba834c9c8f4127f1568e3c49b9c80ca947a57
oskarew <oswa.public@gmail.com>
2025-04-29 11:17:09 +0800

    constants and reusables kept separate and isolated

--- src/utils/globals.py
+++ src/utils/globals.py
@@ -1,5 +1,14 @@
+import torch
+import torch.nn as nn
+from .early_stopping import EarlyStopper
+
 # Constants
 EPOCHS = 100
 LEARNING_RATE = 0.001
 PATIENCE = 5
 BATCH_SIZE = 8
+
+# Reusables
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+early_stopper = EarlyStopper(patience=PATIENCE)
+loss_fn = nn.CrossEntropyLoss()
