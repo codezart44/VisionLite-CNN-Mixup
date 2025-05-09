76e95667a6badedeaf98eea0f861320f4d65c405
oskarew <oswa.public@gmail.com>
2025-05-09 17:17:17 +0800

    updated batch size 8->32 for faster training, epochs 100->200

--- src/utils/globals.py
+++ src/utils/globals.py
@@ -3,12 +3,12 @@ import torch.nn as nn
 from .early_stopping import EarlyStopper
 
 # Constants
-EPOCHS = 100
+EPOCHS = 200
 LEARNING_RATE = 0.001
 PATIENCE = 5
-BATCH_SIZE = 8
+BATCH_SIZE = 32
 
 # Reusables
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-early_stopper = EarlyStopper(patience=PATIENCE)
 loss_fn = nn.CrossEntropyLoss()
+early_stopper = EarlyStopper(patience=PATIENCE)
